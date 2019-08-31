03bb52bf59df55ff5d4acf9eb616ef856bf9fb17
neroark <ns_ark@hotmail.com>
2019-08-31 08:43:59 +0200

    feat: test modified block

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,5 +1,6 @@
 const chalk = require("chalk");
 const bcrypt = require("bcrypt");
+const util = require("util");
 
 // @ts-ignore
 console.log(chalk`{cyan Super crypto currency}`);
@@ -49,11 +50,31 @@ class createChain {
     block.previousHash = this.getLastestBlock().hash;
     this.chain.push(block);
   }
+
+  async isChainLegit() {
+    for (let i = 1; i < this.chain.length - 1; i++) {
+      if (this.chain[i].hash !== this.chain[i + 1].previousHash) {
+        return false;
+      }
+      const compareProm = util.promisify(bcrypt.compare);
+      const textToHash =
+        this.chain[i].index +
+        this.chain[i].date +
+        JSON.stringify(this.chain[i].data);
+      return await compareProm(textToHash, this.chain[i].hash);
+    }
+    return true;
+  }
 }
 
 const myChain = new createChain();
-console.log(myChain);
-
 myChain.addBlock(1, "01/01/2018", { value: 10 });
 myChain.addBlock(2, "01/01/2018", { value: 15 });
 console.log(myChain);
+
+myChain.isChainLegit().then(res => console.log(res));
+
+// change the value of the chain and test it
+myChain.chain[1].data = { value: 5000 };
+console.log(myChain);
+myChain.isChainLegit().then(res => console.log(res)); // false
